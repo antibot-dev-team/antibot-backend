e5f561c0e803db56335ddc550ae7f1f98dac2f29
manmolecular <regwebghost@yandex.ru>
2021-07-31 00:33:26 +0700

    Develop analyzer prototype

--- antibot/http/antibot.go
+++ antibot/http/antibot.go
@@ -6,22 +6,29 @@ import (
 	"github.com/valyala/fasthttp"
 
 	"github.com/antibot-dev-team/antibot-backend/antibot"
+	"github.com/antibot-dev-team/antibot-backend/antibot/analyzer"
 	"github.com/antibot-dev-team/antibot-backend/internal/web"
 )
 
+const validClient = "VALID_CLIENT"
+const invalidClient = "INVALID_CLIENT"
+
 type AntibotHandlers struct {
-	Version string
-	Cfg     *antibot.Config
-	Logger  *logrus.Logger
+	Version  string
+	Cfg      *antibot.Config
+	Logger   *logrus.Logger
+	Analyzer *analyzer.Analyzer
 }
 
 type analyzeResponse struct {
-	Status string `json:"status"`
-	Data   string `json:"data"`
+	// TODO: Think about response format
+	Decision string `json:"decision"`
 }
 
 type analyzeRequest struct {
-	Data string `json:"data"`
+	// TODO: Think about request format
+	UserAgent  string                    `json:"ua"`
+	Properties analyzer.ClientProperties `json:"properties"`
 }
 
 func (a *AntibotHandlers) Analyze(ctx *fasthttp.RequestCtx) error {
@@ -29,8 +36,15 @@ func (a *AntibotHandlers) Analyze(ctx *fasthttp.RequestCtx) error {
 	if err := web.DecodeJSON(ctx, &requestBody); err != nil {
 		return errors.Wrap(err, "undefined data structure")
 	}
+
+	human := a.Analyzer.AnalyzeProperties(requestBody.Properties)
+
+	decision := validClient
+	if !human {
+		decision = invalidClient
+	}
+
 	return web.RespondJSON(ctx, analyzeResponse{
-		Status: "success",
-		Data:   requestBody.Data,
+		Decision: decision,
 	}, fasthttp.StatusOK)
 }
